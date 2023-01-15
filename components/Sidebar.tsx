8d9dd610b1ed6b0ec4c8812f0a58a666155a8dfd
HunterMRocha <hmrocha2@dons.usfca.edu>
2023-01-15 08:52:32 -0800

    deploying

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,10 +1,8 @@
 import React, { useState } from 'react';
-import { NextPage } from 'next';
 import { useRouter } from 'next/router';
 import Link from 'next/link';
 import { AiFillHome, AiOutlineMenu } from 'react-icons/ai';
 import { ImCancelCircle } from 'react-icons/im';
-import { GoogleLogin, GoogleLogout  } from 'react-google-login';
 
 import { useEffect } from 'react'
 import SuggestedAccounts from './SuggestedAccounts';
